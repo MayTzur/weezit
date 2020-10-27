9746e92e92038b6c524718568f1fb4de9264b32b
MayTzur <49557739+MayTzur@users.noreply.github.com>
2020-10-27 12:06:26 +0200

    Update Sentences.js

--- client_side/src/Components/Sentences.js
+++ client_side/src/Components/Sentences.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Text, View, StyleSheet} from 'react-native'
 import * as Font from 'expo-font';
 
