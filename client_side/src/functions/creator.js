6979b9a5651a182c655a81e21a83d8c5d7d98790
MayTzur <49557739+MayTzur@users.noreply.github.com>
2020-10-27 12:14:46 +0200

    Update creator.js

--- client_side/src/functions/creator.js
+++ client_side/src/functions/creator.js
@@ -1,26 +1,21 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { View, StyleSheet, AsyncStorage } from 'react-native';
-import { Linking } from 'expo';
 import SnackBar from 'react-native-snackbar-dialog-options';
-import {firebase, db} from '../Screens/Game/Firebase';
+import { db } from '../Screens/Game/Firebase';
 import { userState } from '../functions/player';
 let ref = db.ref('/games');
 
 export const AndroidPopup = (navigation) => {
     const params = navigation.state.params;
-    //console.log('AndroidPopup params navigation=', data);
     console.log('AndroidPopup params navigation=', params);
     SnackBar.add("It's game alert!\nClick on what you wanna do: ", {
         confirmText: "Let's play 😀",
         id: params.key, 
-        //isStatic: true,
         backgroundColor: '#00838f',
         buttonColor: '#e1f5fe',
         textColor: '#f1f8e9',
         onConfirm: () => {
           console.log('Thank you')
           ref.child(params.key + '/creator').update({ userState: userState.confirmed })
-          SnackBar.dismiss();//Linking.openURL('exp://192.168.8.84:19000/--/game/data.userId/data.key');
+          SnackBar.dismiss();
           navigation.navigate('Board', { id: params.user.ID, key: params.key })         
         },
         cancelText: 'Skip',
@@ -28,16 +23,15 @@ export const AndroidPopup = (navigation) => {
           SnackBar.dismiss(() => SnackBar.show('Would you like to continue receiving game alerts? 🧐',{
             confirmText: 'Yep!',
             id: params.key, 
-            //isStatic: false,
             backgroundColor: '#26c6da',
             buttonColor: '#e1f5fe',
             textColor: '#000000',
-            onConfirm: () => {//just reject this game               
+            onConfirm: () => {             
                 ref.child(params.key + '/creator').update({ userState: userState.rejected })
                 SnackBar.dismiss()
             },
             cancelText: 'Nop..',
-            onCancel: () => {//remove this game               
+            onCancel: () => {              
                 ref.child(params.key + '/creator').update({ userState: userState.remove })
                 SnackBar.dismiss()
             }
@@ -46,7 +40,7 @@ export const AndroidPopup = (navigation) => {
     })
 }
 
-const newFormat = (questionsRes) => { // ---
+const newFormat = (questionsRes) => { 
   console.log('*** newFormat function ***');
   const newFormat = questionsRes.map((questionObj) => {
       const arr = questionObj.incorrect_answers;
@@ -58,7 +52,7 @@ const newFormat = (questionsRes) => { // ---
   return newFormat;
 }
 
-const shuffleOptionArray = (array) => { // ---
+const shuffleOptionArray = (array) => {
   console.log('*** shuffleOptionArray function ***');
   let max = array.length;
   let temp;
