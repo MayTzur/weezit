c75087ae4a13605e9d84350d96aa7910c53f08fc
MayTzur <49557739+MayTzur@users.noreply.github.com>
2020-10-27 13:01:12 +0200

    Update GameActions.js

--- client_side/src/Screens/Game/GameActions.js
+++ client_side/src/Screens/Game/GameActions.js
@@ -1,43 +1,21 @@
-import React from 'react';
 import {firebase, db} from './Firebase';
-//const ref = db.ref('/games');
-//import {listenGame} from './ListenGame';
-//db = firebase.database();
 
-/*    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-    clearInterval(this.timerID); */
 const difficulty = { easy: 'easy', medium: 'medium', hard: 'hard'};
 const category = {Celebrities: 26, Television: 14, Film: 11, Music: 12};
 const STATE = {AWAIT: 1, READY: 2, GAME_OVER: 3, SWITCH: 4, ANSWERED: 5};
 const ref = db.ref('/games');
-//search game in firebase functions:
-const searchWaitingGame = async() => { //check!
-    console.log('searchWaitingGame function:');
-    // const games = await ref.orderByChild('state').limitToFirst(1).equalTo(STATE.AWAIT).once('value', (snapshot) => {
-  
+
+const searchWaitingGame = async() => {   
     const games = await ref.orderByChild('state').equalTo(STATE.AWAIT).once('value', (snapshot) => {
-      console.log('snapshot.val()');
-      console.log(snapshot.toJSON());
       return snapshot.val();
     });
-  
-    console.log(' return games');
-    return games;
+      return games;
   }
 
-  const transAction = async (key, uid, username) => { //check!
-    console.log('transAction function:')
-    
-    console.log('key=', key);
-    console.log('uid=', uid);
-    console.log('username=', username);
+  const transAction = async (key, uid, username) => {
 
       const transResult = await firebase.database().ref(`/games/${key}/`).transaction(
         (game) => {
-          console.log('game', game)
           if(game.joiner){
             return;
           } else {
@@ -50,13 +28,9 @@ const searchWaitingGame = async() => { //check!
             return game;
           }       
         }).catch((error) => { console.log('error ', error)})
-        console.log('transResult=', transResult);
         return transResult;
   }
-//insert game data to firebase:
-  const writeGameData = (gameInfo) => { //check!
-    console.log('writeGameData function: ');
-  
+  const writeGameData = (gameInfo) => {   
     const newGameRef= ref.push();
     newGameRef.set({ 
       creator: gameInfo.creator,
@@ -64,19 +38,14 @@ const searchWaitingGame = async() => { //check!
       questionsList: gameInfo.questionsList
      })
     .catch((error) => { console.log('error ', error) })
-  
-    console.log('newKey: ');
-    console.log(newGameRef.key);
     return newGameRef.key;
   }
-// questions method:
-  const shuffleOptionArray = (array) => { //check!
-    console.log('shuffleOptionArray function:');
+
+  const shuffleOptionArray = (array) => { 
     let max = array.length;
     let temp;
     let index;
   
-    // While there are elements in the array
     while (max > 0) {
         index = Math.floor(Math.random() * max);
         max--;
@@ -87,8 +56,7 @@ const searchWaitingGame = async() => { //check!
     return array;
   }
 
-  const newFormat = (questionsRes) => { //check!
-    console.log('newFormat function:');
+  const newFormat = (questionsRes) => { 
     const newFormat = questionsRes.map((questionObj) => {
         const arr = questionObj.incorrect_answers;
         arr.push(questionObj.correct_answer);
@@ -96,16 +64,11 @@ const searchWaitingGame = async() => { //check!
 
         return { question: questionObj.question, options: options, correct: questionObj.correct_answer }
     })
-    console.log('newFormat');
-    console.log(newFormat);
     return newFormat;
 }
 
-  const fetchQuestion = async() => { //check!
-    console.log('fetchQuestion function:');
-
+  const fetchQuestion = async() => { 
     const questionAPI = `https://opentdb.com/api.php?amount=10&category=${category.Celebrities}&difficulty=${difficulty.easy}&type=multiple`;
-    console.log('questionAPI :'+questionAPI);
 
     try {
         const response = await fetch(questionAPI, {
@@ -114,12 +77,9 @@ const searchWaitingGame = async() => { //check!
                 'Content-Type': 'application/json; charset=UTF-8'
             })
         });
-        console.log('response= ', response);
-        console.log('response.status= ' + response.status);
+          
         const res = await response.json();
-        console.log('res= ', res.results);
         const questions = res.results
-        console.log(questions);
         
         return await newFormat(questions);
     }
