4710cc461b773fea07417acb72323eed5893accc
MayTzur <49557739+MayTzur@users.noreply.github.com>
2020-10-27 12:36:07 +0200

    Update TakePic.jsx

--- client_side/src/Screens/Camera/TakePic.jsx
+++ client_side/src/Screens/Camera/TakePic.jsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { Text, View, StyleSheet, YellowBox, SafeAreaView, TouchableOpacity, Dimensions } from 'react-native';
+import { Text, View, YellowBox, SafeAreaView, TouchableOpacity } from 'react-native';
 import { Camera } from 'expo-camera';
 import { titleConverter } from '../Functions/TitleConverter';
 import { goClarifai } from '../Functions/GoClarifai';
@@ -11,9 +11,8 @@ import { IconButton, Colors } from 'react-native-paper';
 import { CameraTitle } from '../../Componoents/Sentences';
 import * as ImagePicker from 'expo-image-picker';
 import { getBiography } from './GetInformation';
-const { width, height } = Dimensions.get('screen');
-YellowBox.ignoreWarnings(['Setting a timer']);
 
+YellowBox.ignoreWarnings(['Setting a timer']);
 const options = { base64: true, quality: 1 };
 
 export default class Home extends React.Component {
@@ -37,7 +36,6 @@ export default class Home extends React.Component {
             onetxtdidyouknow: '',
             focusedScreen: true,
             user: null,
-            //isModalVisible: true
         }
         this.DidyouknowApi = 'http://proj.ruppin.ac.il/bgroup8/prod/serverSide/api/Search/Facts'
     }
@@ -50,8 +48,8 @@ export default class Home extends React.Component {
         });  
     }
 
-    async componentDidMount() { //check!
-        console.log('componentDidMount function:');
+    async componentDidMount() {
+        //console.log('componentDidMount function:');
         const { status } = await Camera.requestPermissionsAsync();
         this.setState({ hasCameraPermission: status === 'granted' });
 
@@ -67,12 +65,10 @@ export default class Home extends React.Component {
         this.AddDidYouKnow();
     }
 
-    btnOpenGalery = async () => { //check!
+    btnOpenGalery = async () => { 
         console.log('btnOpenGalery function: ');
         try {
             let result = await ImagePicker.launchImageLibraryAsync(options);
-            console.log('result: ', result);
-            console.log('result.cancelled:', result.cancelled);
             if (!result.cancelled) {
                 this.setState({
                     picUri: result.uri,
@@ -84,7 +80,7 @@ export default class Home extends React.Component {
         }
     };
 
-    tmdbData = async (names) => { //check!
+    tmdbData = async (names) => { 
         console.log('tmdbData function:');
         const url = `https://api.themoviedb.org/3/search/person?api_key=b306387c577add422568c903b1cbb052&language=en-US&query=`;
         let info = await Promise.all(
@@ -102,7 +98,6 @@ export default class Home extends React.Component {
         let objects = await Promise.all(
             info.map(async obj => {
                 const biography = await getBiography(obj.results[0].id);
-                console.log('biography=', biography);
                 return {
                     name: obj.results[0].name,
                     id: obj.results[0].id,
@@ -112,26 +107,22 @@ export default class Home extends React.Component {
                 }
             })
         )
-    
         this.toggleModal()
         this.props.navigation.navigate('Matches', { info: objects, userId: this.state.user.ID, moreInfo: this.state.moreInfo })
     }
 
-    getClarifai = async () => { //check!
-        console.log('enter to "goClarifai" func');
+    getClarifai = async () => { 
         const results = await goClarifai(this.state.base64);
         const names = titleConverter(results);
         await Promise.all([...names]);
         this.tmdbData(names);
     }
 
-    btnSnap = async () => { //check!
+    btnSnap = async () => { 
         console.log('btnSnap function: ');
         try {
             if (this.camera) {
                 let result = await this.camera.takePictureAsync(options);
-                console.log('result: ', result);
-                console.log('result.cancelled:', result.cancelled);
                 this.toggleModal()
                 this.setState({
                     picUri: result.uri,
@@ -224,70 +215,5 @@ export default class Home extends React.Component {
         }
     }
 }
-styles = StyleSheet.create({
-//     didYoyKnowModal: {
-//         backgroundColor: Colors.yellow600,
-//         alignSelf: 'center',
-//         width: width,
-//         height: height/3.6,
-//         justifyContent: 'center',
-//         alignItems: 'center',
-//         //paddingHorizontal: 20,
-//         flexWrap: 'wrap',
-//         borderRadius: 10,
-        
-//         //paddingHorizontal: 10,
-//       },
-//   downbtnModal: {
-//     alignItems: 'flex-end',
-//     marginTop: 20,
-//   },
-//       didYoyKnowModalheadTxt: {
-//         color: Colors.cyan800,
-//         fontSize: 20,
-//         textAlign: 'center',
-//         fontFamily: 'serif',
-//       },
-    
-//       didYoyKnowModaBodyText: {
-//         //marginTop: 20,
-//         fontSize: 15,
-//         color: Colors.cyan800,
-//         fontFamily: 'Roboto',
-//         textAlign:"center",
-//       },
-//       didYoyKnowModaBts: {       
-//         flexDirection: "row",
-//         justifyContent: 'space-around', 
-//       },
-//     appName: {
-//         fontFamily: 'LondrinaSolid',
-//         color: '#393f4d',
-//         fontSize: 40,
-//         top: 15,
-//         margin: 25,
-//         bottom: 30
-//     },
-//     container: {
-//         flexWrap: 'wrap',
-//         alignItems: 'center',
-//         justifyContent: 'center',
-//     },
-//     heading: {
-//         height: '8%',
-//         padding: '20px'
-//     },
-//     title: {
-//         color: '#393f4d',
-//         fontSize: 40,
-//         top: 15,
-//         margin: 25,
-//         bottom: 30
-//     },
-//     btn_row: {
-//         flexDirection: 'column',
-//         display: 'flex',
-//         justifyContent: 'center',
-//     },
-})
+
 
